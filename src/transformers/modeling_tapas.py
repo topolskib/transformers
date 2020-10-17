708bf6391907425283579c75cdbb7975c2d7ad22
Rogge Niels <niels.rogge@howest.be>
2020-10-17 15:07:25 +0200

    Added TapasLMHead

--- src/transformers/modeling_tapas.py
+++ src/transformers/modeling_tapas.py
@@ -827,7 +827,11 @@ class TapasForMaskedLM(TapasPreTrainedModel):
         super().__init__(config)
 
         self.tapas = TapasModel(config)
+<<<<<<< HEAD
         self.lm_head = nn.Linear(config.hidden_size, config.vocab_size)
+=======
+        self.cls = TapasLMHead(config)
+>>>>>>> 2eb5b03f... Added TapasLMHead
 
         self.init_weights()
 
@@ -898,6 +902,31 @@ class TapasForMaskedLM(TapasPreTrainedModel):
             hidden_states=outputs.hidden_states,
             attentions=outputs.attentions,
         )
+
+
+class TapasLMHead(nn.Module):
+    """Tapas Head for masked language modeling."""
+
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.layer_norm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+
+        self.decoder = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+        self.bias = nn.Parameter(torch.zeros(config.vocab_size))
+
+        # Need a link between the two variables so that the bias is correctly resized with `resize_token_embeddings`
+        self.decoder.bias = self.bias
+
+    def forward(self, features, **kwargs):
+        x = self.dense(features)
+        x = gelu(x)
+        x = self.layer_norm(x)
+
+        # project back to size of vocabulary with bias
+        x = self.decoder(x)
+
+        return x
 
 
 @add_start_docstrings(
