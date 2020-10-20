1ccca81b391010f6c135599458245e012c99f9d5
Rogge Niels <niels.rogge@howest.be>
2020-10-20 19:13:17 +0200

    Anoter bug fix with fast tokenizers now being separate

--- src/transformers/tokenization_auto.py
+++ src/transformers/tokenization_auto.py
@@ -89,7 +89,7 @@ from .tokenization_reformer import ReformerTokenizer
 from .tokenization_retribert import RetriBertTokenizer
 from .tokenization_roberta import RobertaTokenizer
 from .tokenization_squeezebert import SqueezeBertTokenizer
-from .tokenization_t5 import T5Tokenizer, T5TokenizerFast
+from .tokenization_t5 import T5Tokenizer
 from .tokenization_tapas import TapasTokenizer
 from .tokenization_transfo_xl import TransfoXLTokenizer
 from .tokenization_xlm import XLMTokenizer
