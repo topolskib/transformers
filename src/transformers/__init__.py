8324c58b410eeded7cb1f47619416133bbd8c200
Niels Rogge <niels.rogge1@gmail.com>
2021-02-12 21:34:06 +0100

    More improvements

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -372,14 +372,9 @@ if is_torch_available():
     _import_structure["models.luke"].extend(
         [
             "LUKE_PRETRAINED_MODEL_ARCHIVE_LIST",
-            "LukeForMaskedLM",
-            "LukeForCausalLM",
-            "LukeForMultipleChoice",
-            "LukeForQuestionAnswering",
-            "LukeForSequenceClassification",
-            "LukeForTokenClassification",
             "LukeLayer",
             "LukeModel",
+            "LukeEntityAwareAttentionModel",
             "LukePreTrainedModel",
             "load_tf_weights_in_luke",
         ]
@@ -1479,6 +1474,7 @@ if TYPE_CHECKING:
 
         from .models.luke import (
             LUKE_PRETRAINED_MODEL_ARCHIVE_LIST,
+            LukeEntityAwareAttentionModel,
             LukeForMaskedLM,
             LukeForCausalLM,
             LukeForMultipleChoice,
