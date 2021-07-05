963fee2bf62859adf7fa56a02163dc5cb355bf4b
Niels Rogge <niels.rogge1@gmail.com>
2021-07-05 17:32:52 +0200

    Add another print statement

--- src/transformers/models/segformer/feature_extraction_segformer.py
+++ src/transformers/models/segformer/feature_extraction_segformer.py
@@ -273,7 +273,7 @@ class SegFormerFeatureExtractor(FeatureExtractionMixin, ImageFeatureExtractionMi
         image = self._crop(image, crop_bbox)
 
         if segmentation_map is not None:
-            segmentation_map = self.to_numpy_array(segmentation_map, channel_first=False)
+            segmentation_map = self.to_numpy_array(segmentation_map, rescale=False, channel_first=False)
             segmentation_map = self._crop(segmentation_map, crop_bbox)
             return image, segmentation_map
 
