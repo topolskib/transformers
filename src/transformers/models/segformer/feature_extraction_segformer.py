c69a93fa285e464fa219952c9be1cc6016ab2b01
Niels Rogge <niels.rogge1@gmail.com>
2021-07-28 15:16:47 +0200

    Add whole_inference and slide_inference methods

--- src/transformers/models/segformer/feature_extraction_segformer.py
+++ src/transformers/models/segformer/feature_extraction_segformer.py
@@ -423,3 +423,55 @@ class SegFormerFeatureExtractor(FeatureExtractionMixin, ImageFeatureExtractionMi
         #          encoded_inputs["labels"] = self.pad_segmentation_maps(encoded_inputs["labels"])
 
         return encoded_inputs
+
+    def whole_inference(self, model, pixel_values, size, mode="bilinear", align_corners=False):
+        """Inference with a full image."""
+
+        # forward pass
+        logits = model(pixel_values=pixel_values)
+        # resize logits to given size
+        logits = nn.functional.interpolate(logits, size=size, mode=mode, align_corners=align_corners, warning=False)
+        # apply softmax on class dimension to get probabilities
+        probs = nn.functional.softmax(logits, dim=1)
+
+        return probs
+
+    def slide_inference(self, model, pixel_values, stride, crop_size, size, mode="bilinear", align_corners=False):
+        """
+        Inference by sliding-window with overlap. If h_crop > height or w_crop > width, the small patch will be used to
+        decode without padding.
+        """
+
+        h_stride, w_stride = stride
+        h_crop, w_crop = crop_size
+        batch_size, _, height, width = pixel_values.size()
+        num_classes = model.config.num_labels
+        h_grids = max(height - h_crop + h_stride - 1, 0) // h_stride + 1
+        w_grids = max(width - w_crop + w_stride - 1, 0) // w_stride + 1
+        preds = pixel_values.new_zeros((batch_size, num_classes, height, width))
+        count_mat = pixel_values.new_zeros((batch_size, 1, height, width))
+        for h_idx in range(h_grids):
+            for w_idx in range(w_grids):
+                y1 = h_idx * h_stride
+                x1 = w_idx * w_stride
+                y2 = min(y1 + h_crop, height)
+                x2 = min(x1 + w_crop, width)
+                y1 = max(y2 - h_crop, 0)
+                x1 = max(x2 - w_crop, 0)
+                cropped_pixel_values = pixel_values[:, :, y1:y2, x1:x2]
+                # forward pass to get the logits
+                cropped_logits = model(pixel_values=cropped_pixel_values)
+                logits += nn.functional.pad(
+                    cropped_logits, (int(x1), int(logits.shape[3] - x2), int(y1), int(logits.shape[2] - y2))
+                )
+
+                count_mat[:, :, y1:y2, x1:x2] += 1
+        assert (count_mat == 0).sum() == 0
+
+        logits = logits / count_mat
+        # resize logits to given size
+        logits = nn.functional.interpolate(logits, size=size, mode=mode, align_corners=align_corners, warning=False)
+        # apply softmax on class dimension to get probabilities
+        probs = nn.functional.softmax(logits, dim=1)
+
+        return probs
