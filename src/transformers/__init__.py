5dc05f32a04b30ee5870eb2d72ce52806b5a0bae
Niels Rogge <niels.rogge1@gmail.com>
2021-09-10 12:36:24 +0200

    More improvements

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -1089,11 +1089,7 @@ if is_torch_available():
     _import_structure["models.perceiver"].extend(
         [
             "PERCEIVER_PRETRAINED_MODEL_ARCHIVE_LIST",
-            "PerceiverForMaskedLM",
-            "PerceiverForMultipleChoice",
-            "PerceiverForQuestionAnswering",
-            "PerceiverForSequenceClassification",
-            "PerceiverForTokenClassification",
+            "PerceiverBasicDecoder",
             "PerceiverLayer",
             "PerceiverModel",
             "PerceiverPreTrainedModel",
@@ -2882,11 +2878,7 @@ if TYPE_CHECKING:
         )
         from .models.perceiver import (
             PERCEIVER_PRETRAINED_MODEL_ARCHIVE_LIST,
-            PerceiverForMaskedLM,
-            PerceiverForMultipleChoice,
-            PerceiverForQuestionAnswering,
-            PerceiverForSequenceClassification,
-            PerceiverForTokenClassification,
+            PerceiverBasicDecoder,
             PerceiverLayer,
             PerceiverModel,
             PerceiverPreTrainedModel,
