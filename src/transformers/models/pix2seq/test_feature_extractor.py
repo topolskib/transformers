7d9594ba4272c276dde88b1bdf746a0989c0ac6b
NielsRogge <niels.rogge1@gmail.com>
2022-05-10 12:14:23 +0000

    Improve feature extractor

---
+++ src/transformers/models/pix2seq/test_feature_extractor.py
@@ -0,0 +1,17 @@
+from transformers import Pix2SeqFeatureExtractor
+import torch
+
+feature_extractor = Pix2SeqFeatureExtractor()
+
+pred_seq = torch.tensor([[1113, 1063, 1183, 1274,  175, 1121, 1521, 1292, 1578,  175, 1040,
+        1540, 1581, 1999,  117, 1000, 1000, 1749, 1999,  163, 1085, 1013,
+        1739, 1494,  117, 1000, 1000, 1749, 1999,   30, 1083, 1000, 1749,
+        1999,   30, 1114, 1062, 1183, 1274,   30, 1041, 1540, 1580, 1999,
+          30, 1121, 1521, 1292, 1578,   30]])
+logits = torch.randn(1,50,3000)
+
+pred_class, pred_bbox, pred_score = feature_extractor.decode_object_seq_to_bbox(pred_seq=pred_seq,
+                                                                                logits=logits,
+                                                                                quantization_bins=1000,
+                                                                                coord_vocab_shift=1000,
+                                                                                )
