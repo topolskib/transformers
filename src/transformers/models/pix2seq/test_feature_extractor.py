48854897d5838cb3c3ad31ab135a4a2d12a0c911
NielsRogge <niels.rogge1@gmail.com>
2022-05-13 12:18:46 +0000

    Add encoding of bounding boxes

--- src/transformers/models/pix2seq/test_feature_extractor.py
+++ src/transformers/models/pix2seq/test_feature_extractor.py
@@ -5,65 +5,29 @@ from transformers import Pix2SeqFeatureExtractor
 
 feature_extractor = Pix2SeqFeatureExtractor()
 
-pred_seq = torch.tensor(
+# suppose you have an image, with bounding boxes of shape (1, 100, 4) and labels of shape (1, 100):
+bbox = torch.stack(
     [
-        [
-            1113,
-            1063,
-            1183,
-            1274,
-            175,
-            1121,
-            1521,
-            1292,
-            1578,
-            175,
-            1040,
-            1540,
-            1581,
-            1999,
-            117,
-            1000,
-            1000,
-            1749,
-            1999,
-            163,
-            1085,
-            1013,
-            1739,
-            1494,
-            117,
-            1000,
-            1000,
-            1749,
-            1999,
-            30,
-            1083,
-            1000,
-            1749,
-            1999,
-            30,
-            1114,
-            1062,
-            1183,
-            1274,
-            30,
-            1041,
-            1540,
-            1580,
-            1999,
-            30,
-            1121,
-            1521,
-            1292,
-            1578,
-            30,
-        ]
+        torch.randn(
+            4,
+        )
     ]
-)
+    * 3
+).unsqueeze(0)
+label = torch.tensor([[0, 5, 30]])
+
+print("Shape of bbox:", bbox.shape)
+print("Shape of label:", label.shape)
+
+response_seq, response_seq_class_m, token_weights = feature_extractor.build_response_seq_from_bbox(bbox, label)
+
+print("Response seq:", response_seq)
+
 logits = torch.randn(1, 50, 3000)
 
 pred_class, pred_bbox, pred_score = feature_extractor.decode_object_seq_to_bbox(
-    pred_seq=pred_seq,
+    pred_seq=response_seq,
     logits=logits,
 )
+
+print("Pred class:", pred_class)
