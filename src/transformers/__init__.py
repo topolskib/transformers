fca8c51ea76f4fab88176df2bbfcf13de7960f69
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-11-16 11:14:12 +0100

    More fixes

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -1037,7 +1037,7 @@ else:
     _import_structure["models.git"].extend(
         [
             "GIT_PRETRAINED_MODEL_ARCHIVE_LIST",
-            "GITLMHeadModel",
+            "GITForCausalLM",
             "GITModel",
             "GITPreTrainedModel",
             "GITVisionModel",
@@ -4621,7 +4621,7 @@ if TYPE_CHECKING:
         )
         from .models.git import (
             GIT_PRETRAINED_MODEL_ARCHIVE_LIST,
-            GITLMHeadModel,
+            GITForCausalLM,
             GITModel,
             GITPreTrainedModel,
             GITVisionModel,
