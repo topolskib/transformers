ceb4fcef8c1c2faef29f75e814fe92af0b2d9c34
Niels <niels.rogge1@gmail.com>
2023-04-22 21:16:40 +0200

    Fix docs

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -1928,7 +1928,6 @@ else:
         [
             "MASKRCNN_PRETRAINED_MODEL_ARCHIVE_LIST",
             "MaskRCNNForObjectDetection",
-            "MaskRCNNModel",
             "MaskRCNNPreTrainedModel",
         ]
     )
@@ -5412,7 +5411,6 @@ if TYPE_CHECKING:
         from .models.mask_rcnn import (
             MASKRCNN_PRETRAINED_MODEL_ARCHIVE_LIST,
             MaskRCNNForObjectDetection,
-            MaskRCNNModel,
             MaskRCNNPreTrainedModel,
         )
         from .models.maskformer import (
